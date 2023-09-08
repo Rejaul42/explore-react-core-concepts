478a9f41784dbd7c67bfc458e9836cdf39795bcb
Rejaul42 <rejaulkarimraju91@gmail.com>
2023-09-08 09:16:24 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  function handelClick() {
+    alert('Button 1 click')
+  }
+  const handelClick3 = (num) => {
+    alert(num + 5);
+  }
+
+  return (
+    <>
+      <h1>React core concepts</h1>
+
+      <button onClick={handelClick}>Click Me</button>
+      <button onClick={() => { alert('Button 2 Click') }}>Click 2</button>
+      <button onClick={() => {handelClick3(3)}}>Click button 3</button>
+    </>
+  )
+}
+
+export default App
